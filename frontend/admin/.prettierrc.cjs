4aa8fdca73cb5279d706ac64a3b1f395c72f8f70
NextEcho <NextEcho@outlook.com>
2024-03-28 18:23:33 +0800

    feat(frontend): 完成前端部分后台管理项目的搭建

---
+++ frontend/admin/.prettierrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  tabWidth: 2, // 缩进4个字符
+  printWidth: 80, // 超过多少换行
+  singleQuote: false, // 字符串是否使用单引号，默认为false，使用双引号
+  semi: true, // 行尾是否使用分号，默认为true
+  trailingComma: "all", // 是否使用尾逗号，有三个可选值"<none|es5|all>"
+  arrowParens: "avoid", // x => x
+  jsxBracketSameLine: true,
+  requirePragma: false, // 顶部注释
+  bracketSpacing: false, // 对象中的空格 默认true
+  htmlWhitespaceSensitivity: "ignore",
+};
