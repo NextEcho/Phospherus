1e46cfefaa5a327d2d27f8bca17222873b7a0ed6
NextEcho <nextecho@outlook.com>
2024-05-03 15:40:22 +0800

    feat(frontend): update console

--- frontend/admin/src/layout/Header/index.vue
+++
@@ -1,6 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-    <h1>TopBar</h1>
-</template>
-<style lang="scss" scoped></style>
--- frontend/admin/src/layout/Sider/index.vue
+++
@@ -1,6 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-    <h1>SideBar</h1>
-</template>
-<style lang="scss" scoped></style>
