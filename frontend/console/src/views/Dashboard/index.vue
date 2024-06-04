1754ea6aaa7aade4167894e71ede99445acadadc
NextEcho <nextecho@outlook.com>
2024-06-04 11:10:37 +0800

    重构 frontend 部分

--- frontend/console/src/views/Dashboard/index.vue
+++
@@ -1,7 +0,0 @@
-<script lang="ts" setup></script>
-
-<template>
-    <div>I am Home Page</div>
-</template>
-
-<style lang="scss" scoped></style>
--- frontend/console/src/views/Tag/index.vue
+++
@@ -1,7 +0,0 @@
-<script lang="ts" setup></script>
-
-<template>
-    <div>I am Tag Page</div>
-</template>
-
-<style lang="scss" scoped></style>
