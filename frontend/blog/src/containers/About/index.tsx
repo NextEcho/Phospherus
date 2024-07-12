e662901a7dd3d46a642b98d83c3c0c448138eb3a
NextEcho <nextecho@outlook.com>
2024-07-12 15:20:35 +0800

    feat: finish about page framework

--- frontend/blog/src/containers/About/index.tsx
+++ frontend/blog/src/containers/About/index.tsx
@@ -7,23 +7,33 @@ const About = (): JSX.Element => {
             <div className="about">
                 <NavBar />
                 <div className="w-full h-[64px] bg-nav"></div>
-                <div className="content w-full h-full text-slate-200 min-h-screen"
+                <div className="content w-full h-full text-slate-200 min-h-screen flex flex-col items-center"
                     style={{
                         backgroundColor: "#0E0E13",
-                    }}>
-                    <div className="main flex">
-                        <div className="introduce">
-                            <h1>NextEcho</h1>
-                            <p>
-                                I am currently a product designer, junior developer, and aspire to become a design engineer.
-                                Having insights and practical experience in multiple industries such as cloud computing,
-                                low code, e-commerce, intelligent customer service, and zero trust.
-                            </p>
+                    }}
+                >
+                    <div className="top mx-24 px-80 pt-28 mb-10">
+                        <div className="name text-4xl mb-6 font-serif">
+                            NextEcho
                         </div>
-                        <div className="contact">
-                            <h3>Contact</h3>
+                        <div className="detail flex">
+                            <div className="introduction basis-2/3">
+                                <p className="text-xl mb-6 font-serif">Introduce</p>
+                                <p className="leading-loose">I am currently a product designer, junior developer, and aspire to become a design engineer. Having insights and practical experience in multiple industries such as cloud computing, low code, e-commerce, intelligent customer service, and zero trust.
+                                    I am passionate about the infinite possibilities of technology and also focused on exploring the values of product design, seeking a balance between ultimate design, interaction, technological inclusion, and business understanding to solve problems.
+                                    In addition, I have won three patents for appearance invention, which can be found in the China National Intellectual Property Administration ↗ Enter my name on the official website to search.
+                                </p>
+                            </div>
+                            <div className="contact px-16 basis-1/3">
+                                <p className="text-xl mb-6 font-serif">Contact</p>
+                                <p>
+                                    email: <a href="">nextecho@outlook.com</a>
+                                </p>
+                            </div>
                         </div>
                     </div>
+                    <div className="middle">作品数量统计</div>
+                    <div className="bottom">履历</div>
                 </div>
                 <Footer />
             </div>
