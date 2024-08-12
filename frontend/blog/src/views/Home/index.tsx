87381b6554a527b6217b63c070356dd799e2b952
NextEcho <nextecho@outlook.com>
2024-08-12 23:36:25 +0800

    feat: add blog to console button

--- frontend/blog/src/views/Home/index.tsx
+++ frontend/blog/src/views/Home/index.tsx
@@ -7,77 +7,91 @@ import { getArticleListAPI } from "@/api/article";
 import { articleItem } from "@/api/article/types";
 
 interface ArticleItemProps {
-  id: number;
-  order: number;
-  title: string;
-  time: string;
+    id: number;
+    order: number;
+    title: string;
+    time: string;
 }
 
 const ArticleItem: React.FC<ArticleItemProps> = ({ id, order, title, time }) => {
-  const navigate = useNavigate();
-  const handleClick = (id: number, title: string) => {
-    navigate(`/article/${title}?id=${id}`);
-  };
+    const navigate = useNavigate();
+    const handleClick = (id: number, title: string) => {
+        navigate(`/article/${title}?id=${id}`);
+    };
 
-  return (
-    <div className="article-item w-full py-2 my-2 flex justify-around items-center font-main">
-      <a className="title text-2xl basis-2/3 text-slate-50 min-w-[512px] truncate">
-        <span
-          className="hover:cursor-pointer"
-          onClick={(e) => {
-            e.preventDefault();
-            handleClick(id, title);
-          }}
-        >
-          {order}.{title}
-        </span>
-      </a>
-      <span className="time basis-1/3 text-slate-500 text-right min-w-28 font-code">{time}</span>
-    </div>
-  );
+    return (
+        <div className="article-item w-full py-2 my-2 flex justify-around items-center font-main">
+            <a className="title text-2xl basis-2/3 text-slate-50 min-w-[512px] truncate">
+                <span
+                    className="hover:cursor-pointer"
+                    onClick={(e) => {
+                        e.preventDefault();
+                        handleClick(id, title);
+                    }}
+                >
+                    {order}.{title}
+                </span>
+            </a>
+            <span className="time basis-1/3 text-slate-500 text-right min-w-28 font-code">
+                {time}
+            </span>
+        </div>
+    );
 };
 
 const Home = () => {
-  const [articles, setArticles] = useState<articleItem[]>([] as articleItem[]);
+    const [articles, setArticles] = useState<articleItem[]>([] as articleItem[]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const jsonResp = await getArticleListAPI({ pageNum: 1, pageSize: 10 });
-      const articleListData = jsonResp.data;
+    useEffect(() => {
+        const fetchData = async () => {
+            const jsonResp = await getArticleListAPI({ pageNum: 1, pageSize: 10 });
+            const articleListData = jsonResp.data;
 
-      setArticles(articleListData.articleList);
+            setArticles(articleListData.articleList);
+        };
+
+        fetchData();
+    }, []);
+
+    const articleListData = () => {
+        if (articles.length === 0) {
+            return (
+                <div className="font-main text-slate-50 text-3xl">
+                    <span>暂无文章</span>
+                </div>
+            );
+        }
+
+        return articles.map((item, idx) => {
+            return (
+                <div key={idx}>
+                    <ArticleItem
+                        order={idx + 1}
+                        id={item.id}
+                        title={item.title}
+                        time={item.latestUpdatedAt}
+                    />
+                    <Divider />
+                </div>
+            );
+        });
     };
 
-    fetchData();
-  }, []);
-
-  return (
-    <div className="home-page flex flex-col min-h-screen">
-      <div className="navigation">
-        <NavBar />
-      </div>
-      <div className="content bg-main w-full h-full flex flex-1 flex-col items-center px-96 pt-24 pb-8">
-        <div className="article-list flex flex-col items-center w-5/6 max-w-[80rem] min-w-[720px]">
-          {articles.map((item, idx) => {
-            return (
-              <div key={idx}>
-                <ArticleItem
-                  order={idx + 1}
-                  id={item.id}
-                  title={item.title}
-                  time={item.latestUpdatedAt}
-                />
-                <Divider />
-              </div>
-            );
-          })}
+    return (
+        <div className="home-page flex flex-col min-h-screen">
+            <div className="navigation">
+                <NavBar />
+            </div>
+            <div className="content bg-main w-full h-full flex flex-1 flex-col items-center px-96 pt-24 pb-8">
+                <div className="article-list flex flex-col items-center w-5/6 max-w-[80rem] min-w-[720px]">
+                    {articleListData()}
+                </div>
+            </div>
+            <div className="bottom">
+                <Footer />
+            </div>
         </div>
-      </div>
-      <div className="bottom">
-        <Footer />
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Home;
--- frontend/blog/vite.config.ts
+++ frontend/blog/vite.config.ts
@@ -10,4 +10,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"), // 将 '@' 设置为 'src' 目录的别名
     },
   },
+  server: {
+    port: 10000,
+  },
 });
