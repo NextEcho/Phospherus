427c1d19da3c4982b90b6621aea9c3ced742cd4f
NextEcho <nextecho@outlook.com>
2024-08-25 10:30:29 +0800

    feat: refactor response code

--- backend/global/biz/zcode.go
+++ backend/global/biz/zcode.go
@@ -1,1 +1,10 @@
 package biz
+
+const (
+	CodeSuccess = 0  // 默认的成功响应码
+	CodeFail    = -1 // 默认的错误响应码
+)
+
+const (
+	CodeTokenInvalid = 1000 + iota // token 过期
+)
--- backend/model/common/response/response.go
+++ backend/model/common/response/response.go
@@ -2,6 +2,7 @@ package commonresp
 
 import (
 	"net/http"
+	"phospherus/global/biz"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,13 +12,6 @@ type Response struct {
 	Message string      `json:"message"`
 	Data    interface{} `json:"data"`
 }
-
-// 默认的成功和失败响应码
-// 更多响应码存放于 global/biz/zcode.go 文件中
-const (
-	SUCCESS = 0  // 默认的成功响应 code
-	FAIL    = -1 // 默认的失败响应 code
-)
 
 func Result(ctx *gin.Context, code int, message string, data interface{}) {
 	ctx.JSON(http.StatusOK, Response{
@@ -29,30 +23,35 @@ func Result(ctx *gin.Context, code int, message string, data interface{}) {
 
 // Ok 不带数据的成功响应
 func Ok(ctx *gin.Context) {
-	Result(ctx, SUCCESS, "操作成功", map[string]interface{}{})
+	Result(ctx, biz.CodeSuccess, "操作成功", map[string]interface{}{})
 }
 
 // OkWithData 带有数据的成功响应
 func OkWithData(ctx *gin.Context, data interface{}) {
-	Result(ctx, SUCCESS, "查询成功", data)
+	Result(ctx, biz.CodeSuccess, "查询成功", data)
 }
 
 // OkWithMessage 带有消息的成功响应
 func OkWithMessage(ctx *gin.Context, message string) {
-	Result(ctx, SUCCESS, message, map[string]interface{}{})
+	Result(ctx, biz.CodeSuccess, message, map[string]interface{}{})
 }
 
 // OkWithDetail 既有消息又有数据的成功响应
 func OkWithDetail(ctx *gin.Context, message string, data interface{}) {
-	Result(ctx, SUCCESS, message, data)
+	Result(ctx, biz.CodeSuccess, message, data)
 }
 
 // Fail 不带数据的失败响应
 func Fail(ctx *gin.Context) {
-	Result(ctx, FAIL, "操作失败", map[string]interface{}{})
+	Result(ctx, biz.CodeFail, "操作失败", map[string]interface{}{})
 }
 
 // FailWithMessage 带有消息的失败响应
 func FailWithMessage(ctx *gin.Context, message string) {
-	Result(ctx, FAIL, message, map[string]interface{}{})
+	Result(ctx, biz.CodeFail, message, map[string]interface{}{})
+}
+
+// FailWithCodeAndMessage 带有消息和响应码的失败响应
+func FailWithCodeAndMessage(ctx *gin.Context, code int, message string) {
+	Result(ctx, code, message, map[string]interface{}{})
 }
