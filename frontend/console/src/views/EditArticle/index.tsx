1cbde766760df59607efc14bba34b6780a4cc952
NextEcho <nextecho@outlook.com>
2024-10-02 21:11:02 +0800

    🚩refactor(console): rewrite edit-article page, improve cover selection display of article

--- frontend/console/src/views/EditArticle/index.tsx
+++ frontend/console/src/views/EditArticle/index.tsx
@@ -1,14 +1,15 @@
 import { useCallback, useState } from "react";
-import { Card, message } from "antd";
+import { Card, ConfigProvider, Drawer, message } from "antd";
 import { postArticleAPI } from "@/api/article";
 import { postArticleReq } from "@/api/article/types";
 import { tagItem } from "@/api/tag/types";
 import MDEditor from "@uiw/react-md-editor";
-import ImageUploader from "./ImageUploader";
 import TagSelect from "./TagSelect";
 import PublicSwitcher from "./PublicSwitcher";
 
 const EditArticle = () => {
+    const [openDrawer, setOpenDrawer] = useState(false);
+    const [showAttachmentSelector, setShowAttachmentSelector] = useState<boolean>(false);
     const [title, setTitle] = useState("");
     const [cover, setCover] = useState("");
     const [mdContent, setMdContent] = useState("");
@@ -16,15 +17,7 @@ const EditArticle = () => {
     const [selectedTags, setSelectedTags] = useState<number[]>([]);
     const [tags, setTags] = useState<tagItem[]>([] as tagItem[]);
 
-    const handleEditorChange = (value?: string) => {
-        setMdContent(value || "");
-    };
-
-    const handleSetCover = useCallback((newCover: string) => {
-        setCover(newCover);
-    }, []);
-
-    const handlePostArtcile = (status: number) => {
+    const postArticle = async (status: number) => {
         const params: postArticleReq = {
             authorId: 1,
             title: title,
@@ -33,12 +26,11 @@ const EditArticle = () => {
             isVisible: visible ? 1 : 0,
             tagIds: selectedTags,
             status: status,
-            description: "", // 后端会自动截取，后续可在此处优化，比如使用 AI 自动生成
+            description: "",
         };
 
-        const sendData = async () => {
+        try {
             const jsonResp = await postArticleAPI(params);
-
             if (jsonResp.code === 0) {
                 if (status === 0) {
                     message.success("保存文章成功", 1);
@@ -48,9 +40,17 @@ const EditArticle = () => {
             } else {
                 message.error("出现错误", 1);
             }
-        };
+        } catch (err) {
+            console.log("catch error:", err)
+        }
+    }
 
-        sendData();
+    const handleEditorChange = (value?: string) => {
+        setMdContent(value || "");
+    };
+
+    const handlePostArtcile = (status: number) => {
+        postArticle(status);
     };
 
     return (
@@ -61,37 +61,51 @@ const EditArticle = () => {
                         placeholder="Title of article"
                         value={title}
                         onChange={(e) => setTitle(e.target.value)}
-                        className="peer h-full flex-1 border-b-2 border-gray-200 bg-transparent pt-4 pb-1.5 text-lg font-normal 
+                        className="peer h-full flex-1 border-b-2 border-slate-700 bg-transparent pt-4 pb-1.5 text-lg font-normal 
                 text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-indigo-500 
                 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50 mb-5 font-main text-slate-50 pl-2"
                     />
                     <button className="btn-green my-4 mx-2" onClick={() => handlePostArtcile(1)}>发布文章</button>
-                    <button className="btn-orange my-4" onClick={() => handlePostArtcile(0)}>
+                    <button className="btn-orange my-4" onClick={() => {
+                        setOpenDrawer(true);
+                    }}>
                         保存文章
                     </button>
                 </div>
                 <div className="md-container h-full" data-color-mode="dark">
-                    <MDEditor height={590} value={mdContent} onChange={handleEditorChange} />
+                    <MDEditor className="font-main" height={590} value={mdContent} onChange={handleEditorChange} />
                 </div>
             </Card>
 
-            <div className="flex">
-                <Card className="bg-[#272E48] border-none font-main mt-4 mr-4 w-1/3 h-[110px]">
-                    <ImageUploader onSetCover={handleSetCover} />
-                </Card>
-
-                <Card className="bg-[#272E48] border-none font-main mt-4 text-slate-50 w-1/3 mr-4 h-[110px]">
-                    <TagSelect
-                        selectedTags={selectedTags}
-                        setSelectedTags={setSelectedTags}
-                        tags={tags}
-                        setTags={setTags}
-                    />
-                </Card>
-
-                <Card className="bg-[#272E48] border-none font-main mt-4 text-slate-50 w-1/3 h-[110px]">
-                    <PublicSwitcher visible={visible} setVisible={setVisible} />
-                </Card>
+            <div className="article-properties">
+                <ConfigProvider
+                    theme={{
+                        token: {
+                            colorBgBase: "#272E48",
+                            colorTextBase: "#ffffff",
+                        },
+                    }}
+                >
+                    <Drawer title="发布文章" open={openDrawer} onClose={() => setOpenDrawer(false)} width={430}>
+                        <div className="tag-select text-slate-50 font-main mb-10">
+                            <TagSelect
+                                selectedTags={selectedTags}
+                                setSelectedTags={setSelectedTags}
+                                tags={tags}
+                                setTags={setTags}
+                            />
+                        </div>
+                        <div className="public-switcher text-slate-50 font-main mb-8">
+                            <PublicSwitcher visible={visible} setVisible={setVisible} />
+                        </div>
+                        <div className="image-uploader text-slate-50 font-main flex items-center">
+                            <span className="mr-4">
+                                设置封面:
+                            </span>
+                            <button className="btn-violet" onClick={() => setShowAttachmentSelector(true)}>从附件中选择</button>
+                        </div>
+                    </Drawer>
+                </ConfigProvider>
             </div>
         </div>
     );
