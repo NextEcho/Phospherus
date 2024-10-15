42978c2a1f2a6cabbaa576580c13225ac5f0a19c
NextEcho <nextecho@outlook.com>
2024-10-15 15:39:20 +0800

    fix: remove err that don't affect run of markdown components

--- frontend/blog/src/views/ArticleDetail/index.tsx
+++ frontend/blog/src/views/ArticleDetail/index.tsx
@@ -8,8 +8,25 @@ import { getArticleDetailAPI } from "@/api/article";
 import { useParams } from "react-router-dom";
 import CodeBlock from "@/components/CodeBlock";
 import "@/theme/phospherus.scss";
+import { Components } from 'react-markdown'
 
 const MarkdownRenderer = ({ markdown }: { markdown: string }) => {
+
+    const components: Components = {
+        code({ node, inline, className, children, ...props }: any) {
+            const match = /language-(\w+)/.exec(className || '')
+            return !inline && match ? (
+                <CodeBlock
+                    language={match[1]}
+                    value={String(children).replace(/\n$/, '')}
+                />
+            ) : (
+                <code className={className} {...props}>
+                    {children}
+                </code>
+            )
+        }
+    }
 
     return (
         <ReactMarkdown
@@ -19,21 +36,7 @@ const MarkdownRenderer = ({ markdown }: { markdown: string }) => {
             children={markdown}
             remarkPlugins={[remarkGfm]}
             rehypePlugins={[rehypeRaw]}
-            components={{
-                code({ node, inline, className, children, ...props }) {
-                    const match = /language-(\w+)/.exec(className || '')
-                    return !inline && match ? (
-                        <CodeBlock
-                            language={match[1]}
-                            value={String(children).replace(/\n$/, '')}
-                        />
-                    ) : (
-                        <code className={className} {...props}>
-                            {children}
-                        </code>
-                    )
-                }
-            }}
+            components={components}
         />
     );
 };
